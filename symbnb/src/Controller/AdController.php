39d3a9f02e741277de0fa9bf970bf3fcca424856
Kasimi Karim <karim.kasimi@etudiant.univ-rennes1.fr>
2020-01-08 19:44:31 +0100

    gestion des utilisateurs et liasion avec les annonces

--- symbnb/src/Controller/AdController.php
+++ symbnb/src/Controller/AdController.php
@@ -51,6 +51,8 @@ class AdController extends AbstractController
                 $image->setAd($ad);
                 $manager->persist($image);
             }
+
+            $ad->setAuthor($this->getUser());
             $manager->persist($ad);
             $manager->flush();
 
--- symbnb/src/DataFixtures/AppFixtures.php
+++ symbnb/src/DataFixtures/AppFixtures.php
@@ -4,17 +4,62 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder){
+        
+        $this->encoder=$encoder;
+
+    }
     public function load(ObjectManager $manager)
     {
 
         $faker=Factory::create('FR-fr');
- 
+
+        //Gestion des utilisateurs
+
+        $users=[];
+        $genres = ['male','female'];
+
+
+        for ($i=1; $i <=10 ; $i++) { 
+            $user= new User();
+
+            $genre=$faker->randomElement($genres);
+
+            $picture = 'https://randomuser.me/api/portraits/';
+
+            $pictureId=$faker->numberBetween(1,99). '.jpg';
+
+            if($genre=="male") $picture= $picture. 'men/'.$pictureId;
+            else $picture = $picture . 'women/' .$pictureId;
+
+            $hash = $this->encoder->encodePassword($user, 'password');
+
+            $user->setFirstName($faker->firstname($genre))
+                ->setLastName($faker->lastname)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'. join('</p><p>', $faker->paragraphs(1)).'</p>')
+                ->setHash($hash)
+                ->setPicture($picture);
+
+                $manager->persist($user);
+                $users[]= $user;
+            }
+
+
+
+        //Gestion des annonces 
+
         // $product = new Product();
         // $manager->persist($product);
         for($i=0;$i<30;$i++){
@@ -23,14 +68,17 @@ class AppFixtures extends Fixture
         $title=$faker->sentence();
         $coverImage=$faker->imageUrl(1000,350);
         $introduction=$faker->paragraph(2);
-        $content='<p>'. join('</p><p>', $faker->paragraphs(5)).'</p>';
+        $content='<p>'. join('</p><p>', $faker->paragraphs(2)).'</p>';
+
+        $user = $users[mt_rand(0, count($users)-1)];
 
         $ad->setTitle($title)
             ->setCoverImage($coverImage)
             ->setIntroduction($introduction)
             ->setContent($content)
             ->setPrice(mt_rand(40,200))
-            ->setRooms(mt_rand(1,5));
+            ->setRooms(mt_rand(1,5))
+            ->setAuthor($user);
 
         for ($j=1;$j<=mt_rand(2,5);$j++){
             $image = new Image();
