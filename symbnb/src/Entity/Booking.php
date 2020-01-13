928e2fa0c4a49fe53536d72e98053668d8272df6
Kasimi Karim <karim.kasimi@etudiant.univ-rennes1.fr>
2020-01-13 21:08:40 +0100

    gestion reservation admin finished

--- symbnb/src/Entity/Booking.php
+++ symbnb/src/Entity/Booking.php
@@ -33,7 +33,8 @@ class Booking
     /**
      * @ORM\Column(type="datetime")
      * @Assert\Date(message="Attention, la date d'arrivée doit être au bon format!")
-     * @Assert\GreaterThan("today", message="La date d'arrivée doit être ultérieure à la date d'aujourd'hui")
+     * @Assert\GreaterThan("today", message="La date d'arrivée doit être ultérieure à la date d'aujourd'hui",
+     * groups={"front"})
      */
     private $startDate;
 
@@ -102,6 +103,7 @@ class Booking
     /**
      * Callback appelé à chaque création de réservation
      * @ORM\PrePersist
+     * @ORM\PreUpdate
      */
 
     public function prePersist(){
